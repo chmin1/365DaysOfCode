ed81f507ff3057b9c1242b8abb97f4b904a017b5
Chavane Minto <chavaneminto@gmail.com>
2018-12-28 00:32:16 -0500

    created 365 day view. need to get gradients right

---
+++ YearOfCode/YearViewController.swift
@@ -0,0 +1,74 @@
+//
+//  YearViewController.swift
+//  YearOfCode
+//
+//  Created by Chavane Minto on 12/25/18.
+//  Copyright © 2018 Chavane Minto. All rights reserved.
+//
+
+import UIKit
+
+class YearViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var yearView: UICollectionView!
+    
+    var gradientColors: [UIColor] = []
+    let weeks = 52;
+    let days = 7;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let flowLayout = yearView.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.minimumInteritemSpacing = 5
+        flowLayout.minimumLineSpacing = flowLayout.minimumInteritemSpacing
+        let cellsPerLine: CGFloat = 7;
+        let interItemSpacingTotal = flowLayout.minimumLineSpacing * (cellsPerLine - 1)
+        let width = yearView.frame.size.width / cellsPerLine - interItemSpacingTotal/cellsPerLine;
+        flowLayout.sectionInset = UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0);
+        flowLayout.itemSize = CGSize(width: width, height: width)
+
+        yearView.delegate = self;
+        yearView.dataSource = self;
+        generateColors()
+    }
+    
+    func generateColors() {
+        for i in 0..<weeks {
+            let progress = CGFloat(weeks-i)
+            let newRed = CGFloat((progress/52.0) * 171.0 + CGFloat(i)/52.0 * 178.0) / 255.0
+            let newGreen = CGFloat((progress/52.0) * 143.0 + CGFloat(i)/52.0 * 245.0) / 255.0
+            let newBlue = CGFloat((progress/52.0) * 255.0 + CGFloat(i)/52.0 * 255.0) / 255.0
+            self.gradientColors.append(UIColor(red: newRed,
+                                               green: newGreen,
+                                               blue: newBlue,
+                                               alpha: 1.0))
+        }
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return self.weeks;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.days;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = yearView.dequeueReusableCell(withReuseIdentifier: "yearCell", for: indexPath)
+        cell.backgroundColor = self.gradientColors[indexPath.section]
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
